10e5d156df014850bf6df91e216d6a9d6fa0ec1f
ChangJune1118 <cooljune1234@naver.com>
2025-05-03 21:42:12 +1000

    Create FirstStep.java

---
+++ FirstStep.java
@@ -0,0 +1,18 @@
+public class FirstStep {
+    public class Character {
+		private String name;
+		private int health;
+		private int strength;
+		private int intelligence;
+		private int defense;
+	
+    public Character(String name, int health, int strength, int intelligence, int defense) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.intelligence = intelligence;
+        this.defense = defense;
+    } 
+        
+    }
+}
