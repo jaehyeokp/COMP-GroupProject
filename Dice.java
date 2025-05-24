7463b4cdd643060fdcb6f92bd5c81dc1f0c9b129
HansPark05 <hans2p17@gmail.com>
2025-05-24 17:18:03 +1000

    Hans
    Dice 클래스 작성

--- Dice.java
+++ Dice.java
@@ -1,3 +1,19 @@
 public class Dice {
   // + roll (max: int) int
+   private int DiceNum;
+
+    public Dice(int DiceNum) {
+        this.DiceNum = DiceNum;
+    }
+
+    public int roll() {
+        return (int)(Math.random() * DiceNum) + 1;
+    }
+    
+
+    // 이 부분은 확인용 나중에 지워야함! 
+    public static void main(String[] args) {
+        Dice d1= new Dice(6);
+        System.out.println("Test: "+ d1.roll());
+    }
 }
