44c4430d2cafdc6841a3ad5849d205e6fab15c30
liverblu <foxlvpeace@gmail.com>
2025-06-01 21:03:53 +1000

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,15 +1,12 @@
-/**
- * The Client class initializes characters, assigns them to teams,
- * and starts the RPG turn-based battle.
- */
+//The Client class initializes characters, assigns them to teams,
 public class Client {
     public static void main(String[] args) {
 
         // === Create Team A ===
         Team teamA = new Team("Marvel Comics");
-        teamA.addMember(new Character("Hulk", 30, 16, 2));         // Fighter
+        teamA.addMember(new Character("Hulk", 30, 16, 2));         // Fighter: name, health, strength, defense
         teamA.addMember(new Character("Iron Man", 30, 18, 3));     // Fighter
-        teamA.addMember(new Character("Doctor Strange", 30, 4));   // Healer
+        teamA.addMember(new Character("Doctor Strange", 30, 4));   // Healer : name, health, defense
 
         // === Create Team B ===
         Team teamB = new Team("DC Comics");
@@ -17,12 +14,12 @@ public class Client {
         teamB.addMember(new Character("Superman", 30, 14, 10));    // Fighter
         teamB.addMember(new Character("Joker", 30, 3));            // Healer
 
-        // === Start the Game ===
+        //Start the Game
         System.out.println(" ### Masters of MQ: Turn-Based RPG Combat ### ");
         System.out.println("==============================================");
 
         Battle battle = new Battle(teamA, teamB);
-        battle.startBattle(); // Start the turn-based battle
+        battle.startBattle(); // Call method to begin the battle
 
         System.out.println(" ### Game Over. ### ");
     }
